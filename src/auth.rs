d5c21318f7332d5c1739880721417f39c2455112
Anthony J. Martinez <anthony@ajmartinez.com>
2021-03-29 20:27:01 +0200

    Generate AppConfig from user input
    - Add auth::generate_alphanum_password
    - Use above when generating AppConfig from user input
    - Add TODO doc comments to public interface

--- src/auth.rs
+++ src/auth.rs
@@ -1,1 +1,16 @@
-// Placeholder for authentication
+/// TODO Document
+pub fn generate_alphanum_password(len: usize) -> Result<String, Box<dyn std::error::Error>> {
+    use rand::{thread_rng, Rng,
+	    distributions::Alphanumeric};
+
+    if len < 32 {
+	Err(From::from("Random passwords shorter than 32ch are useless"))
+    } else {
+	let pass: String = thread_rng()
+	    .sample_iter(&Alphanumeric)
+	    .take(len)
+	    .map(char::from)
+	    .collect();
+	Ok(pass)
+    }
+}
--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,12 @@
-use std::io::prelude::*;
+use std::{io::BufRead, usize};
 use std::path::Path;
 
-use clap::{App, AppSettings, Arg, ArgMatches, SubCommand, Values};
+use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
+
 use serde::Deserialize;
 use toml;
 
-
-#[derive(Debug)]
-pub enum Config {
-    Main(AppConfig),
-    New(BuildConfig),
-}
+use crate::auth;
 
 fn args() -> App<'static, 'static> {
     App::new("Caty's Blog")
@@ -26,28 +22,18 @@ fn args() -> App<'static, 'static> {
 			 .index(1)))
 	.subcommand(SubCommand::with_name("new")
 		    .about("Generates a base directory structure and configuration file for a new blog")
-		    .arg(Arg::with_name("username")
-			 .help("Provides the username for the blog's admin")
-			 .required(true)
-			 .takes_value(true)
-			 .short("u")
-			 .long("user"))
-		    .arg(Arg::with_name("topics")
-			 .help("Provides the list of topics for the generator.")
-			 .required(true)
-			 .short("t")
-			 .long("topic")
-			 .multiple(true)
-			 .min_values(1)))
+		    )
 }
 
-pub fn load() -> Result<Config, Box<dyn std::error::Error>> {
+/// TODO Document this public function
+/// And Include an Example of its Use
+pub fn load() -> Result<AppConfig, Box<dyn std::error::Error>> {
     let matches = args().get_matches();
-    let config: Result<Config, Box<dyn std::error::Error>>;
+    let config: Result<AppConfig, Box<dyn std::error::Error>>;
     if matches.is_present("run") {
 	config = runner_config(matches);
     } else if matches.is_present("new") {
-	config = init_config(matches);
+	config = init_config();
     } else {
 	let msg = format!("Unable to load configuration");
 	config = Err(From::from(msg));
@@ -56,54 +42,54 @@ pub fn load() -> Result<Config, Box<dyn std::error::Error>> {
     config
 }
 
-fn runner_config(m: ArgMatches) -> Result<Config, Box<dyn std::error::Error>> {
+fn runner_config(m: ArgMatches) -> Result<AppConfig, Box<dyn std::error::Error>> {
     if let Some(run) = m.subcommand_matches("run") {
 	let value = run.value_of("config").unwrap();
 	let config = AppConfig::new(value)?;
-	Ok(Config::Main(config))
+	Ok(config)
     } else {
 	let msg = format!("Failed to read arguments for 'run' subcommand");
 	Err(From::from(msg))
     }
 }
 
-fn init_config(m: ArgMatches) -> Result<Config, Box<dyn std::error::Error>> {
-    if let Some(new) = m.subcommand_matches("new") {
-	let username = new.value_of("username").unwrap();
-	let topic_values = new.values_of("topics").unwrap();
-	let config = BuildConfig::new(&username, topic_values)?;
-	Ok(Config::New(config))
-    } else {
-	let msg = format!("Failed to read arguments for 'new' subcommand");
-	Err(From::from(msg))
-    }
+fn init_config() -> Result<AppConfig, Box<dyn std::error::Error>> {
+    let reader = std::io::stdin();
+    let mut reader = reader.lock();
+    let config = AppConfig::generate(&mut reader)?;
+    Ok(config)
 }
 
-#[derive(Debug, Deserialize)]
+/// TODO Document
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct Blog {
     name: String,
     author: String,
     topics: Vec<String>,
 }
 
-#[derive(Debug, Deserialize)]
+/// TODO Document
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct Credentials {
     user: String,
     password: String,
 }
 
-#[derive(Debug, Deserialize)]
+/// TODO Document
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct DocPaths {
     templates: String,
     webroot: String,
 }
 
-#[derive(Debug, Deserialize)]
+/// TODO Document
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct LogConfig {
     level: String
 }
 
-#[derive(Debug, Deserialize)]
+/// TODO Document
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct AppConfig {
     blog: Blog,
     creds: Credentials,
@@ -112,72 +98,150 @@ pub struct AppConfig {
 }
 
 impl AppConfig {
-    pub fn new<T>(config: T) -> Result<AppConfig, Box<dyn std::error::Error>>
+    fn new<T>(config: T) -> Result<AppConfig, Box<dyn std::error::Error>>
     where T: AsRef<Path> {
 	let config = std::fs::read_to_string(config)?;
 	let app_config: AppConfig = toml::from_str(&config)?;
 	Ok(app_config)
     }
-}
 
-#[derive(Debug, Deserialize)]
-pub struct BuildConfig {
-    username: String,
-    topics: Vec<String>
-}
+    fn generate<R: BufRead>(reader: &mut R) -> Result<AppConfig, Box<dyn std::error::Error>> {
+	let current_path = std::env::current_dir()?;
+	let current_path = current_path.display();
 
-impl BuildConfig {
-    pub fn new(username: &str, topic_values: Values) -> Result<BuildConfig, Box<dyn std::error::Error>> {
-	let username = username.to_string();
-	let topics: Vec<String> = topic_values.map(|x| String::from(x)).collect();
-	Ok(BuildConfig { username, topics } )
+	let name = Self::get_input("Please enter a name for the blog: ", reader)?;
+	let author = Self::get_input("Please enter the blog author's name: ", reader)?;
+	let topics = Self::get_input("Please enter comma-separated blog topics: ", reader)?;
+	let topics: Vec<String> = topics.split(",")
+	    .map(|s| s
+		 .trim_start_matches(char::is_whitespace)
+		 .trim_end_matches(char::is_whitespace)
+		 .to_string())
+	    .collect();
+	let blog = Blog { name, author, topics };
+
+	let user = Self::get_input("Please enter an username for the blog admin: ", reader)?;
+
+	const PASSWORD_LEN: usize = 32;
+	let password = auth::generate_alphanum_password(PASSWORD_LEN)?;
+	println!("Save this random password for your admin: {}", password);
+
+	let creds = Credentials { user, password };
+
+	let templates = format!("{}/{}/templates", current_path, "blog"); 
+	let webroot = format!("{}/{}/webroot", current_path, "blog"); 
+	let docpaths = DocPaths { templates, webroot };
+
+	let level = format!("INFO");
+	let logging = LogConfig { level };
+	
+	let config = AppConfig {
+	    blog,
+	    creds,
+	    logging,
+	    docpaths,
+	};
+
+	Ok(config)
     }
 
-    pub fn to_app_config<R: BufRead>(&self, src: R) -> Result<AppConfig, Box<dyn std::error::Error>> {
-	
-	Err(From::from("just working on bits"))
+    fn get_input<R: BufRead>(prompt: &str, reader: &mut R) -> Result<String, Box<dyn std::error::Error>> {
+	let mut buf = String::new();
+	println!("{}", prompt);
+	reader.read_line(&mut buf)?;
+	let buf = String::from(buf
+			       .trim_start_matches(char::is_whitespace)
+			       .trim_end_matches(char::is_whitespace));
+	Ok(buf)
     }
 }
 
 #[cfg(test)]
 mod tests{
     use super::*;
+
     #[test]
     fn build_run_config() {
 	let arg_vec = vec!["caty-blog", "run", "./test_files/test-config.toml"];
 	let matches = args().get_matches_from(arg_vec);
 	let config = runner_config(matches);
 	assert!(config.is_ok());
-	let config = config.unwrap();
-	assert!(matches!(config, Config::Main(_)));
     }
 
     #[test]
-    fn build_new_config() {
-	let arg_vec = vec!["caty-blog", "new", "-u", "caty",
-			   "-t", "yoga", "cooking"];
-	let matches = args().get_matches_from(arg_vec);
-	let config = init_config(matches);
-	assert!(config.is_ok());
-	let config = config.unwrap();
-	assert!(matches!(config, Config::New(_)));
-    }
-
-    #[test]
-    // Fix this test to provide input for each "prompt" for information
     fn generate_blog_config() {
-	let src: &[u8] = b"MagicPassword";
-	let arg_vec = vec!["caty-blog", "new", "-u", "caty",
-			   "-t", "yoga", "cooking"];
+	// Setup all target fields
+	let name = format!("Blog Name");
+	let author = format!("Author Name");
+	let topics: Vec<String> = vec![format!("One"), format!("Two"), format!("Three"), format!("And More")];
+	let blog = Blog { name, author, topics };
+
+	let user = format!("admin");
+	let password = format!("MagicPassword");
+	let creds = Credentials { user, password };
+
+	let current_path = std::env::current_dir().unwrap();
+	let current_path = current_path.display();
+	let templates = format!("{}/blog/templates", current_path);
+	let webroot = format!("{}/blog/webroot", current_path);
+	let docpaths = DocPaths { templates, webroot };
+
+	let level = format!("INFO");
+	let logging = LogConfig { level };
+	
+	let reference_config = AppConfig {
+	    blog,
+	    creds,
+	    logging,
+	    docpaths,
+	};
+	
+	let mut src: &[u8] = b"Blog Name\nAuthor Name\nOne, Two, Three, And More\nadmin\nMagicPassword\nINFO\n";
+	let arg_vec = vec!["caty-blog", "new"];
 	let matches = args().get_matches_from(arg_vec);
-	if let Ok(build_config) = init_config(matches) {
-	    match build_config {
-		Config::New(bc) => {
-		    assert!(bc.to_app_config(src).is_ok())
-		},
-		_ => ()
+	if  matches.is_present("new") {
+	    if let Ok(config) = generate(&mut src) {
+		assert_eq!(reference_config, config)
+	    } else {
+		panic!("Failed to generate the reference config")
 	    }
-	    
 	}
+    }
+
+    // Generate function locally for testing AppConfig::get_input
+    fn generate<R: BufRead>(reader: &mut R) -> Result<AppConfig, Box<dyn std::error::Error>> {
+	let current_path = std::env::current_dir()?;
+	let current_path = current_path.display();
+
+	let name = AppConfig::get_input("Please enter a name for the blog: ", reader)?;
+	let author = AppConfig::get_input("Please enter the blog author's name: ", reader)?;
+	let topics = AppConfig::get_input("Please enter comma-separated blog topics: ", reader)?;
+	let topics: Vec<String> = topics.split(",")
+	    .map(|s| s
+		 .trim_start_matches(char::is_whitespace)
+		 .trim_end_matches(char::is_whitespace)
+		 .to_string())
+	    .collect();
+	let blog = Blog { name, author, topics };
+
+	let user = AppConfig::get_input("Please enter an username for the blog admin: ", reader)?;
+	let password = AppConfig::get_input("Please enter a password for the blog admin: ", reader)?;
+	let creds = Credentials { user, password };
+
+	let templates = format!("{}/{}/templates", current_path, "blog"); 
+	let webroot = format!("{}/{}/webroot", current_path, "blog"); 
+	let docpaths = DocPaths { templates, webroot };
+
+	let level = format!("INFO");
+	let logging = LogConfig { level };
+	
+	let config = AppConfig {
+	    blog,
+	    creds,
+	    logging,
+	    docpaths,
+	};
+
+	Ok(config)
     }
 }
