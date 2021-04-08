13c279b3ff476d384cccea932b075e7629810faf
Anthony J. Martinez <anthony@ajmartinez.com>
2021-04-08 20:55:35 +0200

    Fix the default template
    - Closes #9
    - Closes #10

--- src/render/default.rs
+++ src/render/default.rs
@@ -12,7 +12,7 @@ pub const TEMPLATE: &str = r#"
 <center>
 <h1>{{ blog.name }}</h1>
 <nav>
-<a href="./">HOME</a>
+<a href="./">Home</a>
 {%- for topic in blog.topics %}
 <a href="./{{ topic | slugify }}">{{ topic }}</a>
 {%- endfor -%}
@@ -20,9 +20,13 @@ pub const TEMPLATE: &str = r#"
 </center>
 </header>
 <main>
+{% if posts | length == 0 %}
+<h3>Coming Soon!</h3>
+{% else %}
 {%- for post in posts %}
 {{ post }}
 {%- endfor -%}
+{% endif %}
 </main>
 <footer>
 <p>&#169; {{ blog.author }}</p>
