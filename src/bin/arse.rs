5a1a767ef8be3a65e8f6ad19440b34f05d8b775c
Anthony J. Martinez <anthony@ajmartinez.com>
2021-04-11 18:57:55 +0200

    Add logging calls in auth.rs, common.rs, and arse.rs
    Change to block comments for readability

--- src/bin/arse.rs
+++ src/bin/arse.rs
@@ -1,10 +1,12 @@
-// A Rust Site Engine
-// Copyright 2020-2021 Anthony Martinez
-//
-// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, at your option. This file may not be
-// copied, modified, or distributed except according to those terms.
+/*
+A Rust Site Engine
+Copyright 2020-2021 Anthony Martinez
+
+Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+http://opensource.org/licenses/MIT>, at your option. This file may not be
+copied, modified, or distributed except according to those terms.
+*/
 
 use std::net::SocketAddr;
 use std::sync::Arc;
@@ -17,12 +19,18 @@ use routerify::RouterService;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = config::load()?;
-    info!("Configuration loaded");
     let app = Arc::new(config);
+    info!("Configuration loaded");
     
     let router = routes::router(app.clone());
+    info!("Route handlers loaded");
+
     let service = RouterService::new(router).unwrap();
+    info!("Service loaded");
+
     let addr: SocketAddr = "0.0.0.0:9090".parse().unwrap();
+
+    info!("Creating server on: {}", &addr);
     let server = Server::bind(&addr).serve(service);
 
     info!("Running server on: {}", &addr);
